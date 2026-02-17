5821ee5f1b0ab601b66fdfc849688a4f091e9ffc
KKU89 <krishnakantdeewan@gmail.com>
2026-02-17 11:38:34 +0530

    Time: 1 ms (96.82%) | Memory: 42.8 MB (41.51%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -4,21 +4,21 @@ class Solution {
             return x;
         }
         int start =1,end =x/2;
-        int ans=0;
+       
         while(start<=end){
             int mid= start+(end-start)/2;
             long square=(long)mid*mid;
             if(square==x){
                 return mid ;
             }
-            else if(square<x){
-                ans=mid;
+            if(square<x){
+               
                 start=mid+1;
                 
             }else{
                 end=mid-1;
             }
-        }return ans;
+        }return end;
         
     }
 }
