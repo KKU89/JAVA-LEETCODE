99bfc2317a43ba124d6adce550ca6915211cc00e
KKU89 <krishnakantdeewan@gmail.com>
2026-04-04 13:51:47 -0400

    Time: 1 ms (90.38%) | Memory: 46.7 MB (44.70%) - LeetSync

---
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        int size = 0;
+        
+        for (int i = 0; i < nums.length; i += 2) {
+            size += nums[i];
+        }
+        
+        int[] result = new int[size];
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; i += 2) {
+            int freq = nums[i];
+            int val = nums[i + 1];
+            
+            for (int j = 0; j < freq; j++) {
+                result[index++] = val;
+            }
+        }
+        
+        return result;
+    }
+}
