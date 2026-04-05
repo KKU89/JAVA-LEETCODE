94a0bc6be22270b12399591439f5f2413b88a4ac
KKU89 <krishnakantdeewan@gmail.com>
2026-04-05 06:34:18 -0400

    Time: 0 ms (100.00%) | Memory: 43.1 MB (53.49%) - LeetSync

---
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsPattern(int[] arr, int m, int k) {
+        int count=0;
+        for(int i=0;i<arr.length-m;i++){
+            if(arr[i]==arr[i+m]){
+                count++;
+            if(count==m*(k-1)){
+                return true;}
+            }else{
+                count=0;
+            }
+        }return false;
+        
+    }
+}
